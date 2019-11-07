ac94bf93a9d5b50ba318bfbd77cd7f03b2129fdd
joseramirezjimenez <jose.ramirez5019b@gmail.com>
2019-11-07 09:38:21 -0600

    Added in styling for the QuoteBox component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
 import React, { Component } from 'react';
 import './App.css';
 import QuoteBox from './QuoteBox';
+import quotes from './quotes';
 
 class App extends Component {
+  //Setting up the constructor function wo we can set our default state
+  constructor(props) {
+    super(props);
+    this.state = {
+      quote: quotes[0].quote,
+      author: quotes[0].author
+    }
+  }
+
+  //Create a function called getRandomQuote that returns a random quote from the array of quotes. Hint: You'll need to generate a random number between 0 and 1 less than the length of tha array.
+  getRandomQuote() {
+    const randomQuoteIndex = Math.floor(Math.random() * quotes.length);
+    return quotes(randomQuoteIndex);
+  }
+
+  //Function that will be called when the new quote buttom on the QuoteBox component is pressed
+  handleClick = () => {
+    //Generate a random quote
+    const randomQuote = this.getRandomQuote();
+    //Update our state with information from that random quote
+    this.setState({
+      quote: randomQuote.quote,
+      author: randomQuote.author
+    });
+  }
+
+  //Function to generate a random color from R: 0-154, G: 0-154, B: 0-154
+  getRandomColor(upperBound) {
+    const color = `rgb(
+      ${Math.floor(Math.random() * upperBound)},
+      ${Math.floor(Math.random() * upperBound)},
+      ${Math.floor(Math.random() * upperBound)}
+    )`;
+    return color;
+  }
+
   render() {
-    console.log("HI")
     return (
       <div>
-        <QuoteBox /> 
+        <QuoteBox
+          color={this.getRandomColor(155)}
+          quote={this.state.quote}
+          author={this.state.author}
+          handleClick={this.handleClick}
+        />
       </div>
     );
   }
--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'
+import './QuoteBox.css'
 
 class QuoteBox extends Component {
     render() {
+
+        //Set the background color of the HTML element to the random color that is passed in as a prop
+        const html = document.documentElement;
+        html.style.background = this.props.color;
+
         return (
-            <div>
-                <h1>HI</h1>
+            <div style={{ backgroundColor: 'white' }} className="QuoteBox fadeIn">
+                <h1 className="QuoteBox-quote" style={{ color: this.props.color }}>{this.props.quote}</h1>
+
+                <h4 style={{ color: this.props.color }} className="QuoteBox-author fadeIn">
+                    -{this.props.author !== '' ? this.props.author : 'Unknown'}-
+                </h4>
+
+                <button
+                    style={{ backgroundColor: this.props.color }} onClick={this.handleClick} className="QuoteBox-button">
+                    New Quote</button>
             </div>
         );
     }
